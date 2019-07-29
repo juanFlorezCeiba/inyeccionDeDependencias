701630fc1f1c1ed896c3cf6b6a0c4b3b6d1adf87
Juan Florez Ceiba <juan.florez@ceiba.com.co>
2019-07-28 20:09:41 -0500

    Se realiza l a configuración de esquemas de compilación.

--- inyeccionDeDependencias/ViewController.swift
+++ inyeccionDeDependencias/ViewController.swift
@@ -1,18 +1,30 @@
-//
-//  ViewController.swift
-//  inyeccionDeDependencias
-//
-//  Created by Juan Florez Ceiba on 7/26/19.
-//  Copyright © 2019 Juan Florez Ceiba. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewBackground: UIView!
+    @IBOutlet weak var lblEsquemaConfiguracion: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configuracionEsquemas()
+    }
+    
+    private func configuracionEsquemas() {
+        #if Mocks
+        self.configurarVistaPorEsquema(colorBackground: UIColor.orange, nombreEsquema: "Mocks")
+        #elseif Dev
+        self.configurarVistaPorEsquema(colorBackground: UIColor.red, nombreEsquema: "Dev")
+        #else
+        self.configurarVistaPorEsquema(colorBackground: UIColor.lightGray, nombreEsquema: "OTROS")
+        #endif
+    }
+    
+    
+    private func configurarVistaPorEsquema(colorBackground: UIColor, nombreEsquema: String) {
+        self.viewBackground.backgroundColor = colorBackground
+        self.lblEsquemaConfiguracion.text = nombreEsquema
     }
 
 
